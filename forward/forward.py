e505ba0ac26ee85b7032db68f7c98e36af3e2252
Deihim007 <deihim007@gmail.com>
2020-09-07 17:11:53 +0430

    [Forward] Add block command.

--- forward/forward.py
+++ forward/forward.py
@@ -1,8 +1,10 @@
 from redbot.core import commands, checks, Config
 from googletrans import Translator
-import discord, uuid
+import discord
+import uuid
 
 trans = Translator()
+
 
 class Forward(commands.Cog):
     """Forward messages sent to the bot to the bot owner or in a specified channel."""
@@ -17,8 +19,10 @@ class Forward(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-        self.config = Config.get_conf(self, 1398467138476, force_registration=True)
-        default_global = {"toggles": {"botmessages": False}, "destination": None, "reply": {}}
+        self.config = Config.get_conf(
+            self, 1398467138478, force_registration=True)
+        default_global = {"toggles": {"botmessages": False},
+                          "destination": None, "reply": {}, "blocked": {}}
         self.config.register_global(**default_global)
 
     async def _destination(self, msg: str = None, embed: discord.Embed = None):
@@ -42,9 +46,11 @@ class Forward(commands.Cog):
                 else:
                     embeds[0].set_image(url=attachment.url)
             else:
-                attachments_urls.append(f"[{attachment.filename}]({attachment.url})")
+                attachments_urls.append(
+                    f"[{attachment.filename}]({attachment.url})")
         if attachments_urls:
-            embeds[0].add_field(name="Attachments", value="\n".join(attachments_urls))
+            embeds[0].add_field(name="Attachments",
+                                value="\n".join(attachments_urls))
         return embeds
 
     @commands.Cog.listener()
@@ -53,6 +59,10 @@ class Forward(commands.Cog):
             return
         if message.channel.recipient.id in self.bot.owner_ids:
             return
+        async with self.config.blocked() as blocked:
+            userid = str(message.author.id)
+            if userid in blocked:
+                return
         if message.author == self.bot.user:
             async with self.config.toggles() as toggle:
                 if not toggle["botmessages"]:
@@ -60,10 +70,12 @@ class Forward(commands.Cog):
             msg = f"Sent PM to {message.channel.recipient} (`{message.channel.recipient.id}`)"
             if message.embeds:
                 embed = discord.Embed.from_dict(
-                    {**message.embeds[0].to_dict(), "timestamp": str(message.created_at)}
+                    {**message.embeds[0].to_dict(),
+                     "timestamp": str(message.created_at)}
                 )
             else:
-                embed = discord.Embed(description=message.content, timestamp=message.created_at)
+                embed = discord.Embed(
+                    description=message.content, timestamp=message.created_at)
             await self._destination(msg, embed)
         else:
             embeds = []
@@ -143,10 +155,13 @@ class Forward(commands.Cog):
             await user.send(embed=em)
         except discord.Forbidden:
             await ctx.send(
-                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(user)
+                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(
+                    user)
             )
-        em = discord.Embed(colour=discord.Colour.green(), description="Message delivered to {}".format(user)+"\n**"+message+"**")
-        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator+" | "+random_hash, icon_url=ctx.author.avatar_url)
+        em = discord.Embed(colour=discord.Colour.green(
+        ), description="Message delivered to {}".format(user)+"\n**"+message+"**")
+        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator +
+                      " | "+random_hash, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=em)
         async with self.config.reply() as reply:
             reply[str(ctx.author.id)] = user.id
@@ -161,7 +176,8 @@ class Forward(commands.Cog):
         guild.
         """
         translated = trans.translate(message, dest=language)
-        em = discord.Embed(colour=discord.Colour.red(), description="**Translation:**\n"+translated.text)
+        em = discord.Embed(colour=discord.Colour.red(),
+                           description="**Translation:**\n"+translated.text)
         em.add_field(name="Source:", value=message)
 
         # if ctx.bot.user.avatar_url:
@@ -181,14 +197,45 @@ class Forward(commands.Cog):
             await user.send(embed=em)
         except discord.Forbidden:
             await ctx.send(
-                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(user)
+                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(
+                    user)
             )
-        em = discord.Embed(colour=discord.Colour.green(), description="Message delivered to {}".format(user)+"\n**Translation:**\n"+translated.text+"")
-        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator+" | "+random_hash, icon_url=ctx.author.avatar_url)
+        em = discord.Embed(colour=discord.Colour.green(), description="Message delivered to {}".format(
+            user)+"\n**Translation:**\n"+translated.text+"")
+        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator +
+                      " | "+random_hash, icon_url=ctx.author.avatar_url)
         em.add_field(name="Source:", value=message)
         await ctx.send(embed=em)
         async with self.config.reply() as reply:
             reply[str(ctx.author.id)] = user.id
+
+    @commands.command()
+    @commands.guild_only()
+    @checks.guildowner()
+    async def tblock(self, ctx, user: discord.Member):
+        """Blocks a member from sending dm
+        """
+        async with self.config.blocked() as blocked:
+            userid = str(user.id)
+            if userid not in blocked:
+                blocked[userid] = True
+                await ctx.maybe_send_embed("Blocked <@{}> from send messages to the bot.".format(userid))
+            else:
+                await ctx.maybe_send_embed("This user is already blocked.")
+
+    @commands.command()
+    @commands.guild_only()
+    @checks.guildowner()
+    async def tunblock(self, ctx, user: discord.Member):
+        """Blocks a member from sending dm
+        """
+        async with self.config.blocked() as blocked:
+            userid = str(user.id)
+            if userid in blocked:
+                del blocked[userid]
+                await ctx.maybe_send_embed("Unblocked <@{}> from sending messages to the bot.".format(userid))
+            else:
+                await ctx.maybe_send_embed("This user is not blocked.")
 
     @commands.command(name="r")
     @commands.guild_only()
@@ -202,7 +249,6 @@ class Forward(commands.Cog):
 
         user_id = reply[str(ctx.author.id)]
         user = await self.bot.fetch_user(user_id)
-            
 
         em = discord.Embed(colour=discord.Colour.red(), description=message)
 
@@ -223,8 +269,11 @@ class Forward(commands.Cog):
             await user.send(embed=em)
         except discord.Forbidden:
             await ctx.send(
-                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(user)
+                "Oops. I couldn't deliver your message to {}. They most likely have me blocked or DMs closed!".format(
+                    user)
             )
-        em = discord.Embed(colour=discord.Colour.green(), description="Message delivered to {}".format(user)+"\n``"+message+"``")
-        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator+" | "+random_hash, icon_url=ctx.author.avatar_url)
+        em = discord.Embed(colour=discord.Colour.green(
+        ), description="Message delivered to {}".format(user)+"\n``"+message+"``")
+        em.set_footer(text="@"+ctx.author.name+"#"+ctx.author.discriminator +
+                      " | "+random_hash, icon_url=ctx.author.avatar_url)
         await ctx.send(embed=em)
