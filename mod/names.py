6733f6a89df2b37e44e6d3031353ffb8fe638718
Deihim Poornader <deihim007@gmail.com>
2022-06-25 03:38:05 +0430

    Testing mod notes

--- mod/names.py
+++ mod/names.py
@@ -289,6 +289,14 @@ class ModInfo(MixinMeta):
         data.set_author(name=f"{statusemoji} {name}", url=avatar)
         data.set_thumbnail(url=avatar)
 
+        notes = "testing"
+        if notes:
+            data.add_field(
+                name=_("Mod Notes"),
+                value=notes,
+                inline=False,
+            )
+
         await ctx.send(embed=data)
 
     @commands.command()
